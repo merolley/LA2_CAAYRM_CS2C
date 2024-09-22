85414e428ca0cf66ab10d5a480673e9d5eebf024
merolley <merolly.caay11@gmail.com>
2024-09-22 13:21:34 +0800

    initial commit

---
+++ assignment1.js
@@ -0,0 +1,33 @@
+// student 1 information
+let student1 = "Rolly Mae Morales Caay";
+let student1BirthDate = "April 11, 2005";
+let student1BirthPlace = "Villa Hermosa, Sta. Cruz, 2713";
+let student1Address = "Sitio Baliw, Villa Hermosa, Sta. Cruz, 2713";
+let student1CourseAndYear = "Bachelor of Science in Computer Science 2C";
+let student1Dream = "Cybersecurity";
+//output of student 1
+console.log(student1.toUpperCase().concat( " was born ", student1BirthDate, " at ", student1BirthPlace, " and currently living at ", student1Address, ". ", student1.toLowerCase(), " is taking up ", student1CourseAndYear, " and dreams to be ", student1Dream, " after graduation."));
+console.log("\n");
+
+// student 2 information
+let student2 = "Marc Jerick Marbel Urbano";
+let student2BirthDate = "March 10, 2005";
+let student2BirthPlace = "Bagani Tocgo, Candon City, Ilocos Sur, 2710";
+let student2Address = "Bagani Tocgo, Candon City, Ilocos Sur";
+let student2CourseAndYear = "Bachelor of Science in Computer Science 2C";
+let student2Dream = "Full Stack Developer";
+//output of student 2
+console.log(student2.toUpperCase().concat( " was born ", student2BirthDate, " at ", student2BirthPlace, " and currently living at ", student2Address,". ", student2.toLowerCase(), " is taking up ", student2CourseAndYear, " and dreams to be ", student2Dream, " after graduation."));
+console.log("\n");
+
+//student 3 information
+let student3 = "Joshua Morales Toquero"
+let student3BirthDate = "September 9, 2004"
+let student3BirthPlace = "Pio Paroc, Pampanga, Philippines, 2008";
+let student3Address = "Patpata 2nd, Candon City, Ilocos Sur, 2710";
+let student3CourseAndYear = "Bachelor of Science in Computer Science 2C";
+let studentDream3 = "Full Stack Web Developer";
+//output for student 3
+console.log(student3.toUpperCase().concat( " was born ", student3BirthDate, " at ", student3BirthPlace, " and currently living at ", student2Address,". ", student3.toLowerCase(), " is taking up ", student3CourseAndYear, " and dreams to be ", studentDream3, " after graduation."));
+
+
---
+++ assignment2.js
@@ -0,0 +1,29 @@
+//variables a to e
+let a = 25;
+let b = 2;
+let c = 12;
+let d = "15";
+const e = 15;
+
+//displaying the sum of a, b, c, d, and e combined
+console.log(a+b+c+d+e);
+
+//displaying the operations for comparison
+console.log(d>e);
+console.log(d<e);
+console.log(d=>e);
+console.log(d<=e);
+console.log(d===e);
+
+//variables for operations
+let sub = a-b-c-d-e;
+let mul = a*c;
+let power = e**c;
+
+// displaying the resukts of the operations
+console.log(sub);
+console.log(mul);
+console.log(power);
+
+
+
